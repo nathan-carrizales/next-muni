75b7d64c4f662d1be68fb9976cb4cc9dd6bae1c3
Nathan Carrizales <nathanyalcarrizales@gmail.com>
2024-06-30 15:15:27 -0700

    adding setup scripts

---
+++ create_venv.sh
@@ -0,0 +1,1 @@
+python -m venv my_env && source my_env/bin/activate && pip install pandas && pip install requests
---
+++ mission_bus_times.sh
@@ -0,0 +1,1 @@
+source /home/nate/Desktop/next-muni/venv/bin/activate && python  /home/nate/Desktop/next-muni/muni_monitoring.py --id 13328

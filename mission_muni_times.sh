8d05af97c6ddc0afbd0ccf3540590e70236ac803
Nathan Carrizales <nathanyalcarrizales@gmail.com>
2024-06-30 16:09:39 -0700

    adding in caff

--- mission_muni_times.sh
+++ mission_muni_times.sh
@@ -1,1 +1,1 @@
-source /home/nate/Desktop/next-muni/merlin/bin/activate && python  /home/nate/Desktop/next-muni/muni_monitoring.py --id 13327
+/home/nate/Desktop/next-muni/merlin/bin/awake & source /home/nate/Desktop/next-muni/merlin/bin/activate && python  /home/nate/Desktop/next-muni/muni_monitoring.py --id 13327

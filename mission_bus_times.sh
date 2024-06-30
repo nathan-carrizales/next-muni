077a7cd0200836474038cea48076dc42e58f5da2
Nathan Carrizales <nathanyalcarrizales@gmail.com>
2024-06-30 15:19:14 -0700

    fixes

--- mission_bus_times.sh
+++ mission_bus_times.sh
@@ -1,1 +1,1 @@
-source /home/nate/Desktop/next-muni/venv/bin/activate && python  /home/nate/Desktop/next-muni/muni_monitoring.py --id 13328
+source /home/nate/Desktop/next-muni/merlin/bin/activate && python  /home/nate/Desktop/next-muni/muni_monitoring.py --id 13328

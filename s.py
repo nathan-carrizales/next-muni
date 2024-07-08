db049793c6993796b0de72f6762fa2c94d343af0
Nathan Carrizales <ncarrizales@newsunroad.com>
2024-07-07 19:28:16 -0700

    adding formatting changes

--- s.py
+++ s.py
@@ -1,6 +1,7 @@
 import datetime
 import board
 import adafruit_dht
+import numpy as np
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D18)
@@ -8,20 +9,20 @@ dhtDevice = adafruit_dht.DHT22(board.D18)
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT11(board.D18, use_pulseio=False)
 
 
 def get_temperature_and_humidity(foobar):
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
+        temperature_f = np.round(temperature_c * (9 / 5) + 32, 2)
         humidity = dhtDevice.humidity
-        my_text = f'\n Temperature is {temperature_f} F.' + f'\n \n Humidity is {humidity} %.'
+        my_text = f'\n Temperature is {temperature_f} F.' + f'. Humidity is {humidity} %.'
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        my_text = error.args[0]
+        my_text = '\n Error:' + str(error.args[0])
 
     now = (datetime.datetime.utcnow() - datetime.timedelta(hours=7)).strftime('%l:%M%p').replace(" ", "")
 
